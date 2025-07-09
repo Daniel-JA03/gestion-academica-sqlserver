c0c4842d43ab283adb92c830828e02cb51f23a21
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-07-08 21:25:17 -0500

    se creo la estructura inicial de la base de datos con tablas relacionadas

--- gestion_academica.sql
+++ gestion_academica.sql
@@ -3,3 +3,52 @@ GO
 
 USE gestion_academica
 GO
+
+CREATE TABLE alumnos (
+alumno_id INT PRIMARY KEY IDENTITY(1,1),
+dni VARCHAR(8) NOT NULL,
+nombre VARCHAR(100),
+apellido VARCHAR(100),
+correo VARCHAR(100),
+fecha_nacimiento DATE
+)
+GO
+
+CREATE TABLE docentes (
+docente_id INT PRIMARY KEY IDENTITY(1,1),
+dni VARCHAR(8) NOT NULL,
+nombre VARCHAR(100),
+apellido VARCHAR(100),
+correo VARCHAR(100),
+especialidad VARCHAR(100)
+)
+GO
+
+CREATE TABLE cursos (
+curso_id INT PRIMARY KEY IDENTITY(1,1),
+nombre VARCHAR(100),
+descripcion TEXT,
+creditos INT,
+docente_id INT,
+FOREIGN KEY(docente_id) REFERENCES docentes(docente_id)
+)
+GO
+
+CREATE  TABLE matriculas (
+matricula_id INT PRIMARY KEY IDENTITY(1,1),
+alumno_id INT,
+curso_id INT,
+fecha_matricula DATE DEFAULT GETDATE(),
+FOREIGN KEY(alumno_id) REFERENCES alumnos(alumno_id),
+FOREIGN KEY(curso_id) REFERENCES cursos(curso_id)
+)
+GO
+
+CREATE TABLE calificaciones (
+nota_id INT PRIMARY KEY IDENTITY(1,1),
+matricula_id INT,
+nota DECIMAL(10, 2),
+fecha_registro DATE DEFAULT GETDATE()
+FOREIGN KEY(matricula_id) REFERENCES matriculas(matricula_id)
+)
+GO
