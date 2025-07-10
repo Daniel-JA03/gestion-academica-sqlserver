34380d9dc62d7ab83654b14f2b8fd930338d9321
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-07-09 21:09:07 -0500

    se agrego los procedimientos para alumnos

--- procedimientos.sql
+++ procedimientos.sql
@@ -254,3 +254,151 @@ GO
 EXEC sp_eliminarCarrera 21
 --
 ---
+
+-- ======================================
+-- PROCEDIMIENTOS: ALUMNOS
+-- ======================================
+IF OBJECT_ID('sp_agregarAlumno') IS NOT NULL 
+	DROP PROC sp_agregarAlumno
+GO
+-- Agregar un nuevo Alumno
+CREATE PROC sp_agregarAlumno (
+@dni VARCHAR(8),
+@nombre VARCHAR(100),
+@apellido VARCHAR(100),
+@correo VARCHAR(100),
+@fecnac DATE,
+@idcarrera INT
+)
+AS
+BEGIN
+	IF NOT EXISTS (SELECT 1 FROM alumnos WHERE dni = @dni)
+	BEGIN
+		INSERT INTO alumnos (dni, nombre, apellido, correo, fecha_nacimiento, carrera_id)
+		VALUES (@dni, @nombre, @apellido, @correo, @fecnac, @idcarrera)
+
+		PRINT 'Alumno agregado correctamente.'
+	END
+	ELSE
+	BEGIN
+		PRINT 'ERROR: ya existe un alumno con ese DNI'
+	END
+END
+GO
+
+-- PRUEBA PROCEDURE
+EXEC sp_agregarAlumno '94567850', 'Beto', 'Ortiz', 'beto@gmail.com', '2000-05-12', 2
+--
+---
+IF OBJECT_ID('sp_listarAlumnosBack') IS NOT NULL 
+	DROP PROC sp_listarAlumnosBack
+GO
+
+-- Listar Alumnos Backend
+CREATE PROC sp_listarAlumnosBack
+AS
+BEGIN
+	SELECT
+	a.alumno_id,
+	a.dni,
+	a.nombre,
+	a.apellido,
+	a.correo,
+	a.fecha_nacimiento,
+	a.carrera_id
+	FROM alumnos AS a
+END
+GO
+-- PRUEBA PROCEDURE
+EXEC sp_listarAlumnosBack
+--
+---
+IF OBJECT_ID('sp_listarAlumnosFront') IS NOT NULL 
+	DROP PROC sp_listarAlumnosFront
+GO
+
+-- Listar Alumnos Frontend
+CREATE PROC sp_listarAlumnosFront
+AS
+BEGIN
+	SELECT
+	a.alumno_id AS ID,
+	a.dni AS DNI,
+	CONCAT(a.nombre, SPACE(1), a.apellido) AS Alumno,
+	a.correo AS Correo,
+	a.fecha_nacimiento AS FechaNacimiento,
+	c.nombre AS Carrera
+	FROM alumnos AS a
+	JOIN carreras c ON c.carrera_id = a.carrera_id
+	ORDER BY c.nombre ASC
+END
+GO
+-- PRUEBA PROCEDURE
+EXEC sp_listarAlumnosFront
+--
+---
+IF OBJECT_ID('sp_actualizarAlumno') IS NOT NULL 
+	DROP PROC sp_actualizarAlumno
+GO
+
+-- Actualizar Alumno
+CREATE PROC sp_actualizarAlumno (
+@idalumno INT,
+@dni VARCHAR(8),
+@nombre VARCHAR(100),
+@apellido VARCHAR(100),
+@correo VARCHAR(100),
+@fecNac DATE,
+@idcarrera INT
+)
+AS
+BEGIN
+	IF EXISTS (SELECT 1 FROM alumnos WHERE alumno_id = @idalumno)
+	BEGIN
+		UPDATE alumnos
+		SET dni = @dni,
+			nombre = @nombre,
+			apellido = @apellido,
+			correo = @correo,
+			fecha_nacimiento = @fecNac,
+			carrera_id = @idcarrera
+		WHERE alumno_id =  @idalumno
+		PRINT 'Se actualizo el Alumno Correctamente.'
+	END
+	ELSE
+	BEGIN
+		PRINT 'ERROR: El Alumno no existe'
+	END
+END
+GO
+-- PRUEBA PROCEDURE
+EXEC sp_actualizarAlumno 21, '94567850', 'Beto update', 'Perez', 'beto@gmail.com', '2003-05-12', 2
+--
+---
+IF OBJECT_ID('sp_eliminarAlumno') IS NOT NULL 
+	DROP PROC sp_eliminarAlumno
+GO
+
+-- Eliminar Alumno
+CREATE PROC sp_eliminarAlumno (
+@idalumno INT
+)
+AS
+BEGIN
+	IF EXISTS (SELECT 1 FROM alumnos WHERE alumno_id = @idalumno)
+	BEGIN
+		DELETE
+		FROM alumnos
+		WHERE alumno_id = @idalumno
+
+		PRINT 'Se elimino correctamente al Alumno'
+	END
+	ELSE
+	BEGIN
+		PRINT 'ERROR: El Alumno no existe'
+	END
+END
+GO
+
+-- PRUEBA PROCEDURE
+EXEC sp_eliminarAlumno 21
